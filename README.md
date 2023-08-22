30c6a014a249546d3f719aa4c79cfd03e87d9acf
antowkas <antonsokolov.ac@gmail.com>
2023-08-22 16:56:10 +0300

    discord echo bot

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 *Bot for connecting chats from different messengers.*
 
-| Messenger | Assigned to | Reading | Sending | Answering | Forwarding | 
-|-----------|-------------|---------|---------|-----------|------------|
-| Discord   | in progress | &cross; | &cross; | &cross;   | &cross;    |
-| Telegram  | in progress | &cross; | &cross; | &cross;   | &cross;    |
-| VK        | in progress | &cross; | &cross; | &cross;   | &cross;    |
+| Messenger | Assigned to | Reading     | Files   | Sending | Answering | Forwarding | 
+|-----------|-------------|-------------|---------|---------|-----------|------------|
+| Discord   | in progress | &checkmark; | url     | &cross; | &cross;   | &cross;    |
+| Telegram  | in progress | &cross;     | &cross; | &cross; | &cross;   | &cross;    |
+| VK        | in progress | &cross;     | &cross; | &cross; | &cross;   | &cross;    |
+
+
+# TODO:
+- Link channels in one messangers
+
--- discord_bot.py
+++ discord_bot.py
@@ -4,12 +4,40 @@ from config import discord as cfg
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="☺", intents=intents)
+bot = commands.Bot(command_prefix=cfg.PREFIX, intents=intents)
 
 
 @bot.event
 async def on_ready():
     print("Bot is ready!")
+
+
+@bot.event
+async def on_message(message):
+    if message.channel.id != cfg.CHANNEL or message.author.bot:
+        return
+
+    msg = ""
+    if message.content:
+        msg += message.content
+    if message.attachments:  # TODO: normal files not urls
+        msg += '\n' + '\n'.join(map(lambda x: x.url, message.attachments))
+    if not msg:
+        return
+
+    print(message.channel.id)
+
+    # TODO: send "main.py" (Main.send(from="DISCORD", content="", attachments=[]))
+    await send_message(msg)
+
+
+def get_channel():
+    return bot.get_channel(cfg.CHANNEL)
+
+
+async def send_message(message):  # TODO: add attachments
+    channel = get_channel()
+    await channel.send(message)
 
 
 if __name__ == '__main__':
